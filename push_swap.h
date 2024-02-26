6d166de8b06e6658273c95d4bf92422eca10f4bc
Soufyane Marsi <smarsi@e2r10p10.1337.ma>
2024-02-26 12:24:30 +0100

    pish_swap library

---
+++ push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smarsi <smarsi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/24 15:37:00 by smarsi            #+#    #+#             */
+/*   Updated: 2024/02/26 12:12:22 by smarsi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+void	add_numbers_into_linkedlist(char **numbers, t_stack	**stack_a);
+void	check_dup(char **numbers);
+#endif
