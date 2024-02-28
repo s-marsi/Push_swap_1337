abc71155d49f8e234b48bc0925902c70e01be7af
Soufyane Marsi <smarsi@e1r10p2.1337.ma>
2024-02-28 15:59:29 +0100

    save

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: smarsi <smarsi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 15:37:00 by smarsi            #+#    #+#             */
-/*   Updated: 2024/02/26 12:12:22 by smarsi           ###   ########.fr       */
+/*   Updated: 2024/02/28 13:43:55 by smarsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,11 @@
 
 void	add_numbers_into_linkedlist(char **numbers, t_stack	**stack_a);
 void	check_dup(char **numbers);
+void	push_stack(t_stack **src, t_stack **dst, char c);
+void	swap(t_stack **stack, char c);
+void	swap_s(t_stack **stack_a, t_stack **stack_b);
+void	rotate(t_stack **stack, char c);
+void	rotate_r(t_stack **stack_a, t_stack **stack_b);
+void	r_rotate(t_stack **stack, char c);
+void	r_rotate_r(t_stack **stack_a, t_stack **stack_b);
 #endif
