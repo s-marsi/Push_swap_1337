4ed145b0edd6332b7664c7d7dd25447530261cd0
Soufyane Marsi <smarsi@e2r11p10.1337.ma>
2024-03-04 10:21:10 +0100

    main

--- main.c
+++ main.c
@@ -6,45 +6,37 @@
 /*   By: smarsi <smarsi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 15:35:23 by smarsi            #+#    #+#             */
-/*   Updated: 2024/02/27 22:50:30 by smarsi           ###   ########.fr       */
+/*   Updated: 2024/03/04 09:40:44 by smarsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static	void	ft_free(char **s)
+static void	check_is_number_no_dup(char	**numbers)
 {
-	int	i;
-
-	i = 0;
-	if (!s)
-		return ;
-	while (s[i])
-	{
-		free(s[i]);
-		s[i++] = NULL;
-	}
-	free(s);
-	s = NULL;
-}
-
-void	check_is_number_no_dup(char	**numbers)
-{
-	int	i;
-	int	j;
+	int		i;
+	int		j;
+	long long	num;
 
 	i = 0;
 	check_dup(numbers);
 	while (numbers[i])
 	{
+		num = ft_atoi(numbers[i]);
+		if (num < -2147483648 || num > 2147483647)
+		{
+			write(2, "Error\n", 6);
+			exit(1);
+		}
 		j = 0;
 		while (numbers[i] && numbers[i][j])
 		{
-			if (!isdigit(numbers[i][j]) &&
-				!(numbers[i][j++] == '-' && isdigit(numbers[i][j]) &&
-				numbers[i][j] != '0'))
+			if ((numbers[i][j] == '-' || numbers[i][j] == '+') && numbers[i][j + 1])
+				j++;
+			if (!ft_isdigit(numbers[i][j]))
 			{
-				write(2, "Error: Please enter a valid number.\n", 36);
+				ft_free(numbers);
+				write(2, "Error\n", 6);
 				exit(1);
 			}
 			j++;
@@ -53,47 +45,42 @@ void	check_is_number_no_dup(char	**numbers)
 	}
 }
 
-static char	*remove_tab(char *str)
+static char	**check_args(int ac, char *av[])
 {
-	char	*new;
-	char	**tmp;
-	int		i;
+	int	i;
+	int	j;
+	char **split_numbers;
 
-	tmp = ft_split(str, '\t');
-	free(str);
-	new = NULL;
-	i = 0;
-	while (tmp[i])
+	i = 1;
+	while (i < ac)
 	{
-		new = ft_strjoin(new, " ");
-		new = ft_strjoin(new, tmp[i++]);
+		j = 0;
+		while (av[i][j] && (av[i][j] == ' ' || av[i][j] == '\t'))
+			j++;
+		if (!av[i][j])
+		{
+			write(2, "Error\n", 6);
+			exit(1);
+		}
+		i++;
 	}
-	ft_free(tmp);
-	return (new);
+	split_numbers = get_args(ac, av);
+	check_is_number_no_dup(split_numbers);
+	return (split_numbers);
 }
 
-static char	**get_args(int ac, char *av[])
+static void	sort_chooser(t_stack **stack_a, t_stack **stack_b)
 {
-	char	**split_numbers;
-	char	*numbers;
-	int		i;
-
-	numbers = ft_strdup(av[1]);
-	if (ac > 2)
-	{
-		i = 2;
-		while (i < ac)
-		{
-			numbers = ft_strjoin(numbers, " ");
-			numbers = ft_strjoin(numbers, av[i]);
-			i++;
-		}
-	}
-	numbers = remove_tab(numbers);
-	split_numbers = ft_split(numbers, ' ');
-	free(numbers);
-	numbers = NULL;
-	return (split_numbers);
+	if (ft_lstsize(*stack_a) <= 3)
+		sort_three(stack_a);
+	else if (ft_lstsize(*stack_a) <= 4)
+		sort_four(stack_a, stack_b);
+	else if (ft_lstsize(*stack_a) <= 5)
+		sort_five(stack_a, stack_b);
+	else if (ft_lstsize(*stack_a) <= 100)
+		sort_stack(stack_a, stack_b, 15);
+	else if (ft_lstsize(*stack_a) <= 500)
+		sort_stack(stack_a, stack_b, 30);
 }
 
 int	main(int ac, char *av[])
@@ -102,22 +89,19 @@ int	main(int ac, char *av[])
 	t_stack	*stack_a;
 	t_stack	*stack_b;
 
-	stack_a = NULL;
-	if (ac < 2)
+	if (ac >= 2)
 	{
-		write(2, "need more that 1 args", ft_strlen("need more that 1 args"));
-		exit(1);
+		stack_a = NULL;
+		stack_b = NULL;
+		split_numbers = check_args(ac, av);
+		if (!split_numbers)
+			return (0);
+		add_numbers_into_linkedlist(split_numbers, &stack_a);
+		index_list(&stack_a, ft_lstsize(stack_a));
+		sort_chooser(&stack_a, &stack_b);
+		ft_free(split_numbers);
+		ft_lstclear(&stack_a, NULL);
+		ft_lstclear(&stack_b, NULL);
 	}
-	split_numbers = get_args(ac, av);
-	if (!split_numbers)
-	{
-		write(2, "at least one number\n", 23);
-		exit(1);
-	}
-	check_is_number_no_dup(split_numbers);
-	add_numbers_into_linkedlist(split_numbers, &stack_a);
-	ft_free(split_numbers);
-	ft_lstclear(&stack_a, NULL);
-	ft_lstclear(&stack_b, NULL);
 	return (0);
 }
